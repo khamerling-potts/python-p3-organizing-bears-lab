32ded839e271fa9d3d6c2759c2cacabf8b339cd0
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-12-07 15:16:57 -0500

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, sex, age) VALUES ("Mr. Chocolate", "M", 5);
+INSERT INTO bears (name, sex, age) VALUES ("Rowdy", "M", 2);
+INSERT INTO bears (name, sex, age) VALUES ("Tabitha", "F", 13);
+INSERT INTO bears (name, sex, age) VALUES ("Sergeant Brown", "M", 20);
+INSERT INTO bears (name, sex, age) VALUES ("Melissa", "F", 1);
+INSERT INTO bears (name, sex, age) VALUES ("Grinch", "M", 6);
+INSERT INTO bears (name, sex, age) VALUES ("Weny", "F", 30);
+INSERT INTO bears (name, sex, age) VALUES (NULL, "F", 15);
